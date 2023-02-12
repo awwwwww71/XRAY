34ba1b77f10c8e6c59b5cfcff44714ad2f231af0
MasWay <70774862+mymasway@users.noreply.github.com>
2023-02-12 18:57:17 +0700

    Add files via upload

---
+++ xmenu.sh
@@ -0,0 +1,147 @@
+#!/bin/bash
+clear
+echo -e "\e[36m╒════════════════════════════════════════════╕\033[0m"
+echo -e " \E[0;41;36m                 INFO SERVER                \E[0m"
+echo -e "\e[36m╘════════════════════════════════════════════╛\033[0m"
+uphours=`uptime -p | awk '{print $2,$3}' | cut -d , -f1`
+upminutes=`uptime -p | awk '{print $4,$5}' | cut -d , -f1`
+uptimecek=`uptime -p | awk '{print $6,$7}' | cut -d , -f1`
+cekup=`uptime -p | grep -ow "day"`
+IP=$(curl -s ipinfo.io/ip )
+ISPVPS=$( curl -s ipinfo.io/org )
+#clear
+if [ "$cekup" = "day" ]; then
+echo -e "System Uptime   :  $uphours $upminutes $uptimecek"
+else
+echo -e "System Uptime   :  $uphours $upminutes"
+fi
+echo -e "IP-VPS          :  $IPVPS"
+echo -e "ISP-VPS         :  $ISPVPS"
+
+# // Export Color & Information
+export RED='\033[0;31m'
+export GREEN='\033[0;32m'
+export YELLOW='\033[0;33m'
+export BLUE='\033[0;34m'
+export PURPLE='\033[0;35m'
+export CYAN='\033[0;36m'
+export LIGHT='\033[0;37m'
+export NC='\033[0m'
+
+# // nginx
+nginx=$( systemctl status nginx | grep Active | awk '{print $3}' | sed 's/(//g' | sed 's/)//g' )
+if [[ $nginx == "running" ]]; then
+    status_nginx="${GREEN}ON${NC}"
+else
+    status_nginx="${RED}OFF${NC}"
+fi
+
+# // SSH Websocket Proxy
+xray=$(/etc/init.d/ssh xray | grep Active | awk '{print $3}' | cut -d "(" -f2 | cut -d ")" -f1)
+if [[ $xray == "running" ]]; then
+    status_xray="${GREEN}ON${NC}"
+else
+    status_xray="${RED}OFF${NC}"
+fi
+
+# // SSH Websocket Proxy
+runn=$(/etc/init.d/ssh runn | grep Active | awk '{print $3}' | cut -d "(" -f2 | cut -d ")" -f1)
+if [[ $xray == "running" ]]; then
+    status_runn="${GREEN}ON${NC}"
+else
+    status_runn="${RED}OFF${NC}"
+fi
+
+echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
+echo -e "\E[44;1;39m                     ⇱ INFORMASI VPS ⇲                        \E[0m"
+echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
+echo -e ""
+echo -e "□ Sever Uptime        = $( uptime -p  | cut -d " " -f 2-10000 ) "
+echo -e "□ Current Time        = $( date -d "0 days" +"%d-%m-%Y | %X" )"
+echo -e "□ Current Domain      = $( cat /etc/xray/domain )"
+echo -e "□ Server IP           = ${IP}"
+echo -e "□ ISP           = ${ISPVPS}"
+echo -e "□ Clients Name        = $Name"
+echo -e "□ Exfire Script VPS   = $Exp"
+echo -e "□ Time Reboot VPS     = 00:00 ${GREEN}( Jam 12 Malam )${NC}"
+echo -e "□ License Limit       = 3 VPS ${GREEN}( Persatu IP VPS )${NC}"
+echo -e "□ AutoScript By Dev   = XDRG ${GREEN}( MasWayVPN )${NC}"
+
+echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
+echo -e "\E[44;1;39m                     ⇱ STATUS LAYANAN ⇲                       \E[0m"
+echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
+echo -e ""
+echo -e " [ ${GREEN}XRAY${NC} : ${status_xray} ]      [ ${GREEN}NGINX${NC} : ${status_nginx} ]      [ ${GREEN}RUNNING${NC} : ${status_runn} ]"
+echo -e ""
+echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
+echo -e "\E[44;1;39m                     ⇱ MENU LAYANAN ⇲                         \E[0m"
+echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
+echo -e ""
+echo -e " [${GREEN}01${NC}]${RED} •${NC} Buat Semua Akun XRAY $NC      [${GREEN}10${NC}]${RED} • ${NC}Add Domain $NC"
+echo -e " [${GREEN}02${NC}]${RED} •${NC} Hapus Akun XRAY $NC      [${GREEN}11${NC}]${RED} • ${NC}Speedtest $NC"
+echo -e " [${GREEN}03${NC}]${RED} •${NC} Cek Nama Domain $NC     [${GREEN}12${NC}]${RED} • ${NC}Reboot $NC"
+echo -e " [${GREEN}04${NC}]${RED} •${NC} Cek IP Publik $NC  [${GREEN}13${NC}]${RED} • ${NC}Exit Menu $NC"
+echo -e " [${GREEN}05${NC}]${RED} •${NC} Update DLL $NC [${GREEN}14${NC}]${RED} • ${NC}About $NC"
+echo -e " [${GREEN}06${NC}]${RED} •${NC} Xray Update Core $NC [${GREEN}15${NC}]${RED} • ${NC}Pointing Domain $NC"
+echo -e " [${GREEN}07${NC}]${RED} •${NC} Xray Update Script $NC [${GREEN}16${NC}]${RED} • ${NC}Check All Port Service $NC"
+echo -e " [${GREEN}08${NC}]${RED} •${NC} Restart Xray $NC"
+echo -e " [${GREEN}09${NC}]${RED} •${NC} Renew Cert $NC "
+echo -e " ${RED}"
+echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
+echo -e "\E[44;1;39m                     ⇱ MasWay Project ⇲                      \E[0m"
+echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
+echo -e ""
+read -p "     Select From Options [1-16 or x] :  " xmenu
+case $xmenu in 
+1)
+add-akun
+;;
+2)
+delete-akun
+;;
+3)
+cat /etc/xray/domain
+;;
+4)
+curl -s ipinfo.io/ip
+;;
+5)
+updatedll
+;;
+6)
+updatecore
+;;
+7)
+updatesc
+;;
+8)
+restart-xray
+;;
+9)
+certv2ray
+;;
+10)
+add-domain
+;;
+11)
+speedtest
+;;
+12)
+reboot
+;;
+13)
+exit
+;;
+14)
+cat /root/log-install.txt
+;;
+15)
+auto-pointing
+;;
+16)
+cek-port
+;;
+*)
+echo "Input The Correct Number !"
+;;
+esac
