d4ba5afbaecaa8f9f9c6a52b3c2e94b55a88c94d
MasWay <70774862+mymasway@users.noreply.github.com>
2023-02-12 17:00:21 +0700

    Add files via upload

---
+++ ssh/ssh-vpn.sh
@@ -0,0 +1,582 @@
+#!/bin/bash
+# ==========================================
+# Color
+RED='\033[0;31m'
+NC='\033[0m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+LIGHT='\033[0;37m'
+
+# Getting
+MYIP=$(wget -qO- ipinfo.io/ip);
+#MYIP=$(wget -qO- https://ipv4.icanhazip.com);
+MYIP6=$(wget -qO- https://ipv6.icanhazip.com);
+# Link Hosting Kalian
+maswayvpn="raw.githubusercontent.com/mymasway/XRAY/main/ssh"
+
+# Link Hosting Kalian Untuk Stunnel5
+maswayvpnnnn="raw.githubusercontent.com/mymasway/XRAY/main/stunnel5"
+
+maswayvpnnnnn="raw.githubusercontent.com/mymasway/XRAY/main/update"
+# initializing var
+export DEBIAN_FRONTEND=noninteractive
+MYIP=$(wget -qO- ipinfo.io/ip);
+MYIP2="s/xxxxxxxxx/$MYIP/g";
+NET=$(ip -o $ANU -4 route show to default | awk '{print $5}');
+source /etc/os-release
+ver=$VERSION_ID
+
+#detail nama perusahaan
+country=ID
+state=CENTRAL-JAVA
+locality=SUKOHARJO
+organization=GANDRING
+organizationalunit=GANDRING
+commonname=GANDRING
+email=rengganis@gmail.com
+
+# simple password minimal
+wget -O /etc/pam.d/common-password "https://${maswayvpn}/password"
+chmod +x /etc/pam.d/common-password
+
+# Edit file /etc/systemd/system/rc-local.service
+cat > /etc/systemd/system/rc-local.service <<-END
+[Unit]
+Description=/etc/rc.local
+ConditionPathExists=/etc/rc.local
+
+[Service]
+Type=forking
+ExecStart=/etc/rc.local start
+TimeoutSec=0
+StandardOutput=tty
+RemainAfterExit=yes
+SysVStartPriority=99
+
+[Install]
+WantedBy=multi-user.target
+
+END
+
+# nano /etc/rc.local
+cat > /etc/rc.local <<-END
+#!/bin/sh -e
+# rc.local
+# By default this script does nothing.
+exit 0
+END
+
+# Ubah izin akses
+chmod +x /etc/rc.local
+
+# enable rc local
+systemctl daemon-reload
+systemctl enable rc-local.service
+systemctl start rc-local.service
+
+#update
+apt update -y
+apt upgrade -y
+apt dist-upgrade -y
+apt install ssl-cert -y
+apt install ca-certificate -y
+apt-get remove --purge ufw firewalld -y
+apt-get install gawk -y &>/dev/null
+apt-get remove --purge exim4 -y
+apt-get purge apache2* -y
+rm -rf /etc/apache2
+dpkg --configure -a &>/dev/null
+# install wget and curl
+apt -y install wget curl
+
+# Install Requirements Tools
+apt-get install grep -y &>/dev/null
+apt install python3-pip -y
+apt-add-repository universe -y &>/dev/null
+apt-get install software-properties-common -y &>/dev/null
+apt install ruby -y
+apt install python -y
+apt -y install privoxy
+apt install make -y
+apt install cowsay -y
+apt install figlet -y
+apt install lolcat -y
+apt install cmake -y
+apt install ncurses-utils -y
+apt install coreutils -y
+apt install rsyslog -y
+apt install net-tools -y
+apt install zip -y
+apt install unzip -y
+apt install nano -y
+apt install sed -y
+apt install gnupg -y
+apt install gnupg1 -y
+apt install gnupg2 -y
+apt install bc -y
+apt install jq -y
+apt install apt-transport-https -y
+apt install build-essential -y
+apt install dirmngr -y
+apt install libxml-parser-perl -y
+apt install neofetch -y
+apt install git -y
+apt install lsof -y
+apt install libsqlite3-dev -y
+apt install libz-dev -y
+apt install gcc -y
+apt install g++ -y
+apt install libreadline-dev -y
+apt install zlib1g-dev -y
+apt install libssl-dev -y
+gem install lolcat
+apt install jq curl -y
+apt install dnsutils jq -y
+apt-get install tcpdump -y
+apt-get install dsniff -y
+apt install grepcidr -y
+
+# Privoxy Ports
+Privoxy_Port1='3128'
+Privoxy_Port2='3228'
+
+ # Creating Privoxy server config using cat eof tricks
+cd
+cat <<'privoxy' > /etc/privoxy/config
+# My Privoxy Server Config
+user-manual /usr/share/doc/privoxy/user-manual
+confdir /etc/privoxy
+logdir /var/log/privoxy
+filterfile default.filter
+logfile logfile
+listen-address 0.0.0.0:3128
+listen-address 127.0.0.1:3228
+toggle 1
+enable-remote-toggle 1
+enable-remote-http-toggle 1
+enable-edit-actions 1
+enforce-blocks 1
+buffer-limit 4096
+enable-proxy-authentication-forwarding 1
+forwarded-connect-retries 1
+accept-intercepted-requests 1
+allow-cgi-request-crunching 1
+split-large-forms 0
+keep-alive-timeout 5
+tolerate-pipelining 1
+socket-timeout 300
+permit-access 0.0.0.0/0 IP-ADDRESS
+privoxy
+IP-ADDRESS=$MYIP
+
+#Setting machine's IP Address inside of our privoxy config(security that only allows this machine to use this proxy server)
+sed -i "s|IP-ADDRESS|$MYIP|g" /etc/privoxy/config
+ 
+#Setting privoxy ports
+sed -i "s|Privoxy_Port1|$Privoxy_Port1|g" /etc/privoxy/config
+sed -i "s|Privoxy_Port2|$Privoxy_Port2|g" /etc/privoxy/config
+
+# set time GMT +7
+ln -fs /usr/share/zoneinfo/Asia/Jakarta /etc/localtime
+
+# set locale
+sed -i 's/AcceptEnv/#AcceptEnv/g' /etc/ssh/sshd_config
+mkdir /etc/ssl/zerossl.my.id/
+
+# install
+apt-get --reinstall --fix-missing install -y bzip2 gzip coreutils wget screen rsyslog iftop htop net-tools zip unzip wget net-tools curl nano sed screen gnupg gnupg1 bc apt-transport-https build-essential dirmngr libxml-parser-perl neofetch git lsof
+#echo "neofetch" >> .profile
+echo "status" >> .profile
+
+# install webserver
+apt -y install nginx php php-fpm php-cli php-mysql libxml-parser-perl
+rm /etc/nginx/sites-enabled/default >/dev/null 2>&1
+rm /etc/nginx/sites-available/default >/dev/null 2>&1
+curl https://${maswayvpn}/nginx.conf > /etc/nginx/nginx.conf
+curl https://${maswayvpn}/vps.conf > /etc/nginx/conf.d/vps.conf
+sed -i 's/listen = \/var\/run\/php-fpm.sock/listen = 127.0.0.1:9000/g' /etc/php/fpm/pool.d/www.conf
+useradd -m vps;
+mkdir -p /home/vps/public_html
+echo "<?php phpinfo() ?>" > /home/vps/public_html/info.php
+chown -R www-data:www-data /home/vps/public_html
+chmod -R g+rw /home/vps/public_html
+cd /home/vps/public_html
+wget -O /home/vps/public_html/index.html "https://${maswayvpn}/index.html"
+/etc/init.d/nginx restart
+
+# setting port ssh
+sed -i 's/Port 22/Port 22/g' /etc/ssh/sshd_config
+sed -i '/Port 22/a Port 5015' /etc/ssh/sshd_config
+echo "Port 22" >> /etc/ssh/sshd_config
+echo "Port 5015" >> /etc/ssh/sshd_config
+/etc/init.d/ssh restart
+
+# install dropbear
+apt -y install dropbear
+sed -i 's/NO_START=1/NO_START=0/g' /etc/default/dropbear
+sed -i 's/DROPBEAR_PORT=22/DROPBEAR_PORT=300/g' /etc/default/dropbear
+sed -i 's/DROPBEAR_EXTRA_ARGS=/DROPBEAR_EXTRA_ARGS="-p 200 -p 250"/g' /etc/default/dropbear
+echo "/bin/false" >> /etc/shells
+echo "/usr/sbin/nologin" >> /etc/shells
+/etc/init.d/dropbear restart
+
+# install squid
+cd
+apt -y install squid
+wget -O /etc/squid/squid.conf "https://${maswayvpn}/squid3.conf"
+sed -i $MYIP2 /etc/squid/squid.conf
+
+cat > /lib/systemd/system/squid.service << EOF
+## Copyright (C) 1996-2019 The Squid Software Foundation and contributors
+##
+## Squid software is distributed under GPLv2+ license and includes
+## contributions from numerous individuals and organizations.
+## Please see the COPYING and CONTRIBUTORS files for details.
+##
+
+[Unit]
+Description=SQUID4 WEB PROXY ACTIVATED BY SHANUM
+Documentation=man:squid(8)
+After=network.target network-online.target nss-lookup.target
+
+[Service]
+Type=forking
+PIDFile=/var/run/squid.pid
+ExecStartPre=/usr/sbin/squid --foreground -z
+ExecStart=/usr/sbin/squid -sYC
+ExecReload=/bin/kill -HUP $MAINPID
+KillMode=mixed
+
+[Install]
+WantedBy=multi-user.target
+
+EOF
+
+systemctl daemon-reload
+systemctl enable squid
+
+# Install SSLH
+apt -y install sslh
+rm -f /etc/default/sslh
+
+# Settings SSLH
+cat > /etc/default/sslh <<-END
+# Default options for sslh initscript
+# sourced by /etc/init.d/sslh
+
+# Disabled by default, to force yourself
+# to read the configuration:
+# - /usr/share/doc/sslh/README.Debian (quick start)
+# - /usr/share/doc/sslh/README, at "Configuration" section
+# - sslh(8) via "man sslh" for more configuration details.
+# Once configuration ready, you *must* set RUN to yes here
+# and try to start sslh (standalone mode only)
+
+RUN=yes
+
+# binary to use: forked (sslh) or single-thread (sslh-select) version
+# systemd users: don't forget to modify /lib/systemd/system/sslh.service
+DAEMON=/usr/sbin/sslh
+
+DAEMON_OPTS="--user sslh --listen 0.0.0.0:2053 --ssl 127.0.0.1:500 --ssh 127.0.0.1:200 --openvpn 127.0.0.1:650 --openvpn 127.0.0.1:600 --pidfile /var/run/sslh/sslh.pid"
+
+END
+
+# Restart Service SSLH
+service sslh restart
+systemctl restart sslh
+/etc/init.d/sslh restart
+/etc/init.d/sslh status
+/etc/init.d/sslh restart
+
+# setting vnstat
+apt -y install vnstat
+/etc/init.d/vnstat restart
+apt -y install libsqlite3-dev
+wget https://humdi.net/vnstat/vnstat-2.6.tar.gz
+tar zxvf vnstat-2.6.tar.gz
+cd vnstat-2.6
+./configure --prefix=/usr --sysconfdir=/etc && make && make install
+cd
+vnstat -u -i $NET
+sed -i 's/Interface "'""eth0""'"/Interface "'""$NET""'"/g' /etc/vnstat.conf
+chown vnstat:vnstat /var/lib/vnstat -R
+systemctl enable vnstat
+/etc/init.d/vnstat restart
+rm -f /root/vnstat-2.6.tar.gz
+rm -rf /root/vnstat-2.6
+
+# install stunnel 5 
+cd /root/
+wget -q -O stunnel5.zip "https://${maswayvpnnnn}/stunnel5.zip"
+unzip -o stunnel5.zip
+cd /root/stunnel
+chmod +x configure
+./configure
+make
+make install
+cd /root
+rm -r -f stunnel
+rm -f stunnel5.zip
+mkdir -p /etc/stunnel5
+chmod 644 /etc/stunnel5
+
+# Download Config Stunnel5
+cat > /etc/stunnel5/stunnel5.conf <<-END
+cert = /etc/stunnel5/stunnel5.pem
+client = no
+socket = a:SO_REUSEADDR=1
+socket = l:TCP_NODELAY=1
+socket = r:TCP_NODELAY=1
+[openvpn]
+accept = 900
+connect = 127.0.0.1:2053
+
+[openssh]
+accept = 500
+connect = 127.0.0.1:2053
+
+[stunnelws]
+accept = 555
+connect = 650
+
+[stunnelws]
+accept = 222
+connect = 700
+
+END
+
+# make a certificate
+openssl genrsa -out key.pem 2048
+#openssl req -new -x509 -key key.pem -out cert.pem -days 3650 \
+openssl x509 -req -days 36500 -in ia.csr -TA ta.key -TAkey ta.key -CA ca.crt -CAkey ca.key -set_serial 01 -out -key key.pem -out cert.pem \
+-subj "/C=$country/ST=$state/L=$locality/O=$organization/OU=$organizationalunit/CN=$commonname/emailAddress=$email"
+cat key.pem cert.pem >> /etc/stunnel5/stunnel5.pem
+
+# Service Stunnel5 systemctl restart stunnel5
+cat > /etc/systemd/system/stunnel5.service << END
+[Unit]
+Description=STUNNEL5 ACTIVATED BY WISNUCOKROSATRIO
+Documentation=https://stunnel.org
+Documentation=https://t.me/zerossl
+After=syslog.target network-online.target
+
+[Service]
+ExecStart=/usr/local/bin/stunnel5 /etc/stunnel5/stunnel5.conf
+Type=forking
+
+[Install]
+WantedBy=multi-user.target
+END
+
+# Service Stunnel5 /etc/init.d/stunnel5
+wget -q -O /etc/init.d/stunnel5 "https://${maswayvpnnnn}/stunnel5.init"
+
+# Ubah Izin Akses
+chmod 600 /etc/stunnel5/stunnel5.pem
+chmod +x /etc/init.d/stunnel5
+cp /usr/local/bin/stunnel /usr/local/bin/stunnel5
+
+# Remove File
+rm -r -f /usr/local/share/doc/stunnel/
+rm -r -f /usr/local/etc/stunnel/
+rm -f /usr/local/bin/stunnel
+rm -f /usr/local/bin/stunnel3
+rm -f /usr/local/bin/stunnel4
+#rm -f /usr/local/bin/stunnel5
+
+# Restart Stunnel 5
+systemctl stop stunnel5
+systemctl enable stunnel5
+systemctl start stunnel5
+systemctl restart stunnel5
+/etc/init.d/stunnel5 restart
+/etc/init.d/stunnel5 status
+/etc/init.d/stunnel5 restart
+
+#OpenVPN
+wget https://${maswayvpn}/vpn.sh &&  chmod +x vpn.sh && ./vpn.sh
+
+# install fail2ban
+apt -y install fail2ban
+
+# Instal DDOS Flate
+if [ -d '/usr/local/ddos' ]; then
+	echo; echo; echo "Please un-install the previous version first"
+	exit 0
+else
+	mkdir /usr/local/ddos
+fi
+clear
+echo; echo 'Installing DOS-Deflate 0.6'; echo
+echo; echo -n 'Downloading source files...'
+wet -q -O /usr/local/ddos/ddos.conf http://www.inetbase.com/scripts/ddos/ddos.conf
+echo -n '.'
+wget -q -O /usr/local/ddos/LICENSE http://www.inetbase.com/scripts/ddos/LICENSE
+echo -n '.'
+wget -q -O /usr/local/ddos/ignore.ip.list http://www.inetbase.com/scripts/ddos/ignore.ip.list
+echo -n '.'
+wget -q -O /usr/local/ddos/ddos.sh http://www.inetbase.com/scripts/ddos/ddos.sh
+chmod 0755 /usr/local/ddos/ddos.sh
+cp -s /usr/local/ddos/ddos.sh /usr/local/sbin/ddos
+echo '...done'
+echo; echo -n 'Creating cron to run script every minute.....(Default setting)'
+/usr/local/ddos/ddos.sh --cron > /dev/null 2>&1
+echo '.....done'
+echo; echo 'Installation has completed.'
+echo 'Config file is at /usr/local/ddos/ddos.conf'
+echo 'Please send in your comments and/or suggestions to zaf@vsnl.com'
+# Install DDoS Deflate
+apt install -y dnsutils tcpdump dsniff grepcidr
+wget -qO ddos.zip "https://raw.githubusercontent.com/Hanxhin/Autoscript/main/FILES/ddos-deflate.zip"
+unzip ddos.zip
+cd ddos-deflate
+chmod +x install.sh
+./install.sh
+cd
+rm -rf ddos.zip ddos-deflate
+
+# banner /etc/issue.net
+echo "Banner /etc/issue.net" >>/etc/ssh/sshd_config
+sed -i 's@DROPBEAR_BANNER=""@DROPBEAR_BANNER="/etc/issue.net"@g' /etc/default/dropbear
+
+# Install BBR
+wget https://${maswayvpn}/bbr.sh && chmod +x bbr.sh && ./bbr.sh
+
+# Ganti Banner
+wget -O /etc/issue.net "https://${maswayvpn}/issue.net"
+
+# blockir torrent
+iptables -A FORWARD -m string --string "get_peers" --algo bm -j DROP
+iptables -A FORWARD -m string --string "announce_peer" --algo bm -j DROP
+iptables -A FORWARD -m string --string "find_node" --algo bm -j DROP
+iptables -A FORWARD -m string --algo bm --string "BitTorrent" -j DROP
+iptables -A FORWARD -m string --algo bm --string "BitTorrent protocol" -j DROP
+iptables -A FORWARD -m string --algo bm --string "peer_id=" -j DROP
+iptables -A FORWARD -m string --algo bm --string ".torrent" -j DROP
+iptables -A FORWARD -m string --algo bm --string "announce.php?passkey=" -j DROP
+iptables -A FORWARD -m string --algo bm --string "torrent" -j DROP
+iptables -A FORWARD -m string --algo bm --string "announce" -j DROP
+iptables -A FORWARD -m string --algo bm --string "info_hash" -j DROP
+iptables-save > /etc/iptables.up.rules
+iptables-restore -t < /etc/iptables.up.rules
+netfilter-persistent save
+netfilter-persistent reload
+
+# download script
+cd /usr/bin
+wget -O restart "https://${maswayvpn}/restart.sh"
+wget -O addhost "https://${maswayvpn}/addhost.sh"
+wget -O addssh "https://${maswayvpn}/addssh.sh"
+wget -O trialssh "https://${maswayvpn}/trialssh.sh"
+wget -O menuu "https://${maswayvpn}/menuu.sh"
+wget -O delssh "https://${maswayvpn}/delssh.sh"
+wget -O member "https://${maswayvpn}/member.sh"
+wget -O delexp "https://${maswayvpn}/delexp.sh"
+wget -O cekssh "https://${maswayvpn}/cekssh.sh"
+wget -O renewssh "https://${maswayvpn}/renewssh.sh"
+wget -O autokill "https://${maswayvpn}/autokill.sh"
+wget -O ceklim "https://${maswayvpn}/ceklim.sh"
+wget -O tendang "https://${maswayvpn}/tendang.sh"
+wget -O clearlog "https://${maswayvpn}/clearlog.sh"
+wget -O changeport "https://${maswayvpn}/changeport.sh"
+wget -O wbmn "https://${maswayvpn}/webmin.sh"
+wget -O xp "https://${maswayvpn}/xp.sh"
+wget -O swapkvm "https://${maswayvpn}/swapkvm.sh"
+#wget -O ins-sshws "https://${maswayvpn}/ins-sshws.sh"
+#wget -O sshws-true "https://${maswayvpn}/sshws-true.sh"
+wget -O portovpn "https://${maswayvpn}/portovpn.sh"
+wget -O portsquid "https://${maswayvpn}/portsquid.sh"
+wget -O portvlm "https://${maswayvpn}/portvlm.sh"
+wget -O portstunnel5 "https://${maswayvpn}/portstunnel5.sh"
+wget -O portdropbear "https://${maswayvpn}/portdropbear.sh"
+wget -O portopenssh "https://${maswayvpn}/portopenssh.sh"
+wget -O portsshnontls "https://${maswayvpn}/portsshnontls.sh"
+wget -O portsshwstls "https://${maswayvpn}/portsshwstls.sh"
+wget -O sshovpnmenu "https://${maswayvpnnnnn}/sshovpnmenu.sh"
+wget -O setmenu "https://${maswayvpnnnnn}/setmenu.sh"
+wget -O menutester "https://${maswayvpnnnnn}/menutester.sh"
+wget -O menu "https://${maswayvpnnnnn}/menu.sh"
+wget -O status "https://${maswayvpnnnnn}/status.sh"
+
+chmod +x restart
+chmod +x addhost
+chmod +x addssh
+chmod +x trialssh
+chmod +x menuu
+chmod +x delssh
+chmod +x member
+chmod +x delexp
+chmod +x cekssh
+chmod +x renewssh
+chmod +x autokill
+chmod +x ceklim
+chmod +x tendang
+chmod +x clearlog
+chmod +x changeport
+chmod +x webmin
+chmod +x xp
+chmod +x swapkvm
+chmod +x portsquid
+chmod +x portvlm 
+chmod +x portovpn
+chmod +x portstunnel5
+chmod +x portdropbear
+chmod +x portopenssh
+chmod +x portsshnontls.sh
+chmod +x sshovpnmenu
+chmod +x setmenu
+chmod +x menutester
+chmod +x menu
+chmod +x status
+chmod +x portsshwstls
+
+# remove unnecessary files
+cd
+apt autoclean -y
+apt -y remove --purge unscd
+apt-get -y --purge remove samba*;
+apt-get -y --purge remove apache2*;
+apt-get -y --purge remove bind9*;
+apt-get -y remove sendmail*
+apt autoremove -y
+# finishing
+cd
+chown -R www-data:www-data /home/vps/public_html
+
+# install badvpn
+cd
+wget -O /usr/bin/badvpn-udpgw "https://${maswayvpn}/badvpn-udpgw64"
+chmod +x /usr/bin/badvpn-udpgw
+sed -i '$ i\screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:99' /etc/rc.local
+
+/etc/init.d/nginx restart
+/etc/init.d/openvpn restart
+/etc/init.d/cron restart
+/etc/init.d/ssh restart
+/etc/init.d/dropbear restart
+/etc/init.d/fail2ban restart
+/etc/init.d/sslh restart
+/etc/init.d/stunnel5 restart
+/etc/init.d/vnstat restart
+/etc/init.d/fail2ban restart
+/etc/init.d/squid restart
+screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:99
+echo "0 4 * * * root clearlog && reboot" >> /etc/crontab
+echo "0 0 * * * root xp" >> /etc/crontab
+echo "0 0 * * * root delexp" >> /etc/crontab
+
+service cron restart >/dev/null 2>&1
+service cron reload >/dev/null 2>&1
+history -c
+echo "unset HISTFILE" >> /etc/profile
+
+cd
+rm -f /root/key.pem
+rm -f /root/cert.pem
+rm -f /root/ssh-vpn.sh
+# finishing
+clear
