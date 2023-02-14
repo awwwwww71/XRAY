72c535aee020252e0bea0ba82cc9c1f226e4fc37
MasWay <70774862+mymasway@users.noreply.github.com>
2023-02-15 01:05:06 +0700

    Update install-xray.sh

--- install-xray.sh
+++ install-xray.sh
@@ -328,7 +328,7 @@ cat > /etc/xray/config.json << END
 }
 END
 rm -rf /etc/systemd/system/xray.service.d
-cat > /etc/systemd/system/xray.service
+cat > /etc/systemd/system/xray.service <<EOF
 Description=Xray Service
 Documentation=https://github.com/xtls
 After=network.target nss-lookup.target
